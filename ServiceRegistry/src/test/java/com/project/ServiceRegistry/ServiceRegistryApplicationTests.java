f422a5963fea088b94d728837bb374d0a7534935
Prakarsh <jhethia@gmail.com>
2023-08-25 22:02:51 +0530

    Added Eureka Service Discovery And Config Server

---
+++ ServiceRegistry/src/test/java/com/project/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
